f94b221730be1711e35260f27a076fa12a39a0c6
gurkiratsingh23 <ggreatsingh501@gmail.com>
2024-10-08 00:52:34 +0530

    commit oct

---
+++ javascipt.js
@@ -0,0 +1,13 @@
+// function myFun(){
+//     alert("Hello there!");
+//  }
+ 
+// function myFun(){
+//     document.getElementById('id1').style.color = 'red';
+//  }
+
+ function myFun(){
+    location.href = "page 2.html";
+    
+ }
+
